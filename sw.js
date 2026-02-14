3f97576d974b88949b839dadf376f2ca5879649d
mayckmotta <44411226+mayckmotta@users.noreply.github.com>
2026-02-14 11:59:19 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'portal-cursos-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/data.js',
+  './js/ui.js',
+  './js/search.js',
+  './manifest.json',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+// Instala o Service Worker e armazena os arquivos no cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Intercepta as requisições para servir os arquivos do cache quando offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
