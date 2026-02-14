a240cccd6d16b11dd80f427a4d28f8778b8161db
mayckmotta <44411226+mayckmotta@users.noreply.github.com>
2026-02-14 12:57:32 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,18 @@
-const CACHE_NAME = 'portal-cursos-v2'; // Mude de v1 para v2
-const ASSETS = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/data.js',
-  './js/ui.js',
-  './js/search.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
-];
+const CACHE_NAME = 'portal-v-debug-' + Date.now(); // Gera um nome único sempre
 
-// Limpa caches antigos quando a nova versão ativa
+self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Força o novo Service Worker a assumir na hora
+});
+
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
+      return Promise.all(keys.map((k) => caches.delete(k))); // Apaga TUDO que for velho
     })
   );
 });
 
-// Instala o Service Worker e armazena os arquivos no cache
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-// Intercepta as requisições para servir os arquivos do cache quando offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  // Busca na rede primeiro, se falhar, tenta o cache
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
